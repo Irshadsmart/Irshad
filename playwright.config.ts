beff71b226a9809c57936d340562b55226806d6c
Irshad <irshadsmartone@gmail.com>
2026-03-11 16:57:21 +0530

    Enable headless mode for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   workers: 4,
-
   testDir: './tests',
-
   reporter: 'html',
 
   use: {
     baseURL: 'https://brightbud.ai',
-    headless: false,
-
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    headless: true,   // important for CI
     trace: 'on-first-retry'
   }
-
 });
